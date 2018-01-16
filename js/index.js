e4f6c89366e721b2d9ad39d150f473077e6c45fb
wudiljx <13994367734@163.com>
2018-01-16 18:41:51 +0800

    0116

--- js/index.js
+++ js/index.js
@@ -3,4 +3,12 @@ $(function(){
     $("body").on("click",".playBtn",function(){
         $(".playBgBox").hide();
     })
+    $.fn.nnp3d = function(options){
+    	alert("haha");
+    	var thisOb = this ;
+    	var obj = $.extend({}, $.fn.simple3D.defaults, options );
+    	$(function(){
+    		alert(thisOb.length)
+    	})
+    }
 })
